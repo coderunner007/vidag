d72e7918a8e94921626e3ff69314da057a621fb2
Alan Joseph <alanjos@amazon.com>
2021-03-23 22:26:26 +0530

    [P-2] Add arrows & labels to the nodes
    * TODO: P0
       * zoom-in/out
       * pan in/out
    * Final UI:
       * Add filter?
       * beautify output?
       * Hover nodes and find targets

--- src/js/directedGraph.js
+++ src/js/directedGraph.js
@@ -18,21 +18,20 @@ const init = (data) => {
   const links = data.links.map(d => Object.create(d));
   const nodes = data.nodes.map(d => Object.create(d));
 
-  svgElementSize();
-
   const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("link", d3.forceLink(links).id(d => d.id).distance(200))
     .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("center", d3.forceCenter(width / 2, height / 2))
 
   const svg = d3.select("svg");
 
   const link = svg.append("g")
     .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
+    .attr("stroke-opacity", 0.2)
+    .attr("marker-end", "url(#arrow)")
     .selectAll("line")
     .data(links)
-    .join("line")
+    .enter().append("line")
     .attr("stroke-width", d => Math.sqrt(d.value));
 
   const node = svg.append("g")
@@ -40,20 +39,49 @@ const init = (data) => {
     .attr("stroke-width", 1.5)
     .selectAll("circle")
     .data(nodes)
-    .join("circle")
-    .attr("r", d => (d.count || 0) + 5)
+    .enter().append("circle")
+    .attr("r", d => (d.count || 0) + 6)
     .attr("fill", colour)
     .call(drag(simulation));
 
-  node.append("text")
-    .text(function(d) {
-      return d.id;
+  node.on("mouseover", (d) => {
+    link.style('stroke-opacity', function(l) {
+      if (d === l.target) return 1;
+      else                return 0.05;
     })
-    .attr('x', 6)
-    .attr('y', 3);
+  }).on("mouseout", (d) => {
+    link.style("stroke-opacity", 0.2)
+  });
+
+  svg.append("defs").append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 20)
+    .attr("refY", 0)
+    .attr("opacity", 0.2)
+    .attr("markerWidth", 8)
+    .attr("markerHeight", 8)
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
 
   node.append("title")
     .text(d => d.id);
+
+  const text = svg.append("g").attr("class", "labels").selectAll("g")
+    .data(nodes)
+    .enter().append("g");
+
+  text.append("text")
+    .attr("x", 14)
+    .attr("y", ".31em")
+    .attr("opacity", 0.2)
+    .attr("pointer-events", "none")
+    .style("font-family", "sans-serif")
+    .style("font-size", "0.7em")
+    .text(function(d) { return d.id; })
+  svgElementSize();
+
 
   simulation.on("tick", () => {
     link
@@ -65,6 +93,10 @@ const init = (data) => {
     node
       .attr("cx", d => d.x)
       .attr("cy", d => d.y);
+
+    text
+      .attr("transform",
+        function(d) { return "translate(" + d.x + "," + d.y + ")"; })
   });
 
   // invalidation.then(() => simulation.stop());
